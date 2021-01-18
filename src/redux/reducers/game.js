dab5b8560f83db0c3865f558bbba88228eb2dcbf
Przemysław Gwoździej <przemyslaw.gwozdziej@inspeerity.com>
2021-01-18 14:42:02 +0100

    fix game end check

--- src/redux/reducers/game.js
+++ src/redux/reducers/game.js
@@ -11,7 +11,7 @@ const gameReducer = createReducer(initialState, {
   [generateBoard.type]: () => ({...initialState, board: generateGameBoard()}),
   [addHit.type]: (state, { payload }) => {
     state.hits.push(payload)
-    if (POSSIBLE_KEYS.length === 8) {
+    if (state.hits.length === POSSIBLE_KEYS.length) {
       state.isFinished = true
     }
   }
